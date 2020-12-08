4b0403fcca729b1ab70cd32b3bc21199a2d9fb51
Ibsa Kedir <ibsakedir02@gmail.com>
2020-12-08 11:19:26 +0100

    comitting ini

---
+++ Ibsa.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const death = document.getElementById("death");
+btn.addEventListener('click',  Deathtime)
+
+
+function Deathtime(){
+    const deical = input.value *Math.floor((Math.random() *3));
+    death.textContent = deical;  
+}
+
+console.log(Math.floor(Math.random()*3+1))
+
+
+//1. Brukeren skriver alderen og sj;n
